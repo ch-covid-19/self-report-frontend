1a18cad50398e144d7923343af0fac437a9652a7
Christian Müller <christian.muller@wifx.net>
2020-04-06 22:00:29 +0200

    Fix page height

--- src/pages/Report.vue
+++ src/pages/Report.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-
     <div class="position-relative">
       <!-- shape Hero -->
-      <section class="section-hero section-shaped my-0">
+      <section class="section-shaped my-0">
         <div class="shape shape-style-1 shape-default shape-skew">
-          <span></span>
-          <span></span>
         </div>
         <div class="container shape-container d-flex">
 
